554aa96d45e8a838e012b9c2c9d35cb905c0e411
Raheem Abolfathzadeh <raheemabol91@gmail.com>
2025-03-05 12:22:43 -0500

    *

---
+++ src/main/java/org/rma/springmvcdemo/model/CartItem.java
@@ -0,0 +1,29 @@
+package org.rma.springmvcdemo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int quantity;
+    private double price;
+
+    public double getTotalPrice() {
+        return quantity * price;
+    }
+
+}
